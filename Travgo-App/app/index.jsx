eac8ae962fed22ba7d4ae32987a95f3cf4c0b6c5
Rohit kumar <rohitprj.1282@gmail.com>
2024-06-05 17:21:07 +0530

    done

--- Travgo-App/app/index.jsx
+++ Travgo-App/app/index.jsx
@@ -1,9 +1,52 @@
-import { View } from 'react-native'
+import { Image, Pressable, ScrollView, Text, View } from 'react-native'
+const menu = require("@/assets/images/menu.png")
+const darkMode = require("@/assets/images/sleep-mode.png")
+const user = require("@/assets/images/user.png")
+const slide = require("@/assets/images/slide.png")
+const forward = require("@/assets/images/right-arrow.png")
 
-export default function App(){
-    return(
-        <View>
-            
-        </View>
+
+export default function App() {
+    return (
+        <ScrollView>
+            <View style={{
+                backgroundColor: "#2c79f5",
+                width: "100%",
+                height: 60,
+                flexDirection: "row"
+            }}>
+                <View>
+                    <Pressable>
+                        <Image source={menu} style={{ tintColor: "white", margin: 15 }} />
+                    </Pressable>
+                </View>
+                <View style={{ alignSelf: "center" }}>
+                    <Pressable>
+                        <Image source={darkMode} style={{ tintColor: "white", marginLeft: 190, height: 30, width: 30, }} />
+                    </Pressable>
+
+                </View>
+                <View style={{ alignSelf: "center" }}>
+                    <Pressable>
+                        <Image source={user} style={{ tintColor: "white", marginLeft: 25 }} />
+                    </Pressable>
+                </View>
+            </View>
+            <View>
+                <Image source={slide} style={{ borderBottomRightRadius: 140, borderBottomLeftRadius: 100 }} />
+            </View>
+            <View>
+                <Text style={{ fontWeight: 500, fontSize: 26, marginLeft: 25, marginTop: 20 }}>Best travel destinations in</Text>
+                <Text style={{ fontWeight: 500, fontSize: 26, marginLeft: 125 }}>the world</Text>
+
+                <Text style={{ color: "grey", marginTop: 15, marginLeft: 50 }}>Semper in cursus magna et eu varius nunc  </Text>
+                <Text style={{ color: "grey", marginLeft: 35 }}>adipiscing. Elementum justo, laoreet id semiru </Text>
+                <Text style={{ color: "grey", marginLeft: 145 }}>forigive you.</Text>
+            </View>
+            <View style={{ flexDirection: "row" }}>
+                <Image source={forward} style={{ tintColor: "#2c79f5", height: 70, width: 70, marginBottom: 40, marginTop: 200, marginLeft: 30 }} />
+                <Text style={{ color: "grey", marginTop: 225, marginLeft: 15 }}>Skip</Text>
+            </View>
+        </ScrollView>
     )
 }
